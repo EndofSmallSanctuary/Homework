7e753afce9f81262c5391025dfd5af9f5b292bd2
Sergei Kravchenkov <teenagefeverhotline@gmail.com>
2021-09-04 21:56:27 +0300

    04.09.2021

--- Task2/main.py
+++ Task2/main.py
@@ -1,6 +1,15 @@
+from os import truncate
 from flask import Flask
+from threading import Thread
 from flask.globals import request
+from flask.scaffold import F
 app = Flask(__name__)
+
+
+def initiateapp80():
+    app.run(debug=True,threaded=True,host="0.0.0.0",port=80,use_reloader=False)
+def initiateapp8080():
+    app.run(debug=True,threaded=True,host="0.0.0.0",port=443,use_reloader=False,ssl_context=('certificate/cert.pem', 'certificate/key.pem'))
 
 
 @app.route("/",methods=['GET', 'POST'])
@@ -13,4 +22,7 @@ def request80():
 
 
 if __name__=="__main__":
-    app.run("localhost",80)
+   thread1 = Thread(target=initiateapp80)
+   thread2 = Thread(target=initiateapp8080)
+   thread1.start()
+   thread2.start()
