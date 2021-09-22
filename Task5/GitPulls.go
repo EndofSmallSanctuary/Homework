fe78ba79b915ad74c619c6b0f1c45a03e20cbc52
Sergei Kravchenkov <teenagefeverhotline@gmail.com>
2021-09-22 19:21:47 +0300

    finalizing html

--- Task5/GitPulls.go
+++ Task5/GitPulls.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	b64 "encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -18,8 +19,9 @@ type Gitpulls struct {
 }
 
 type GitUsers struct {
-	Id    int32  `json:"id"`
-	Login string `json:"login"`
+	Id     int32  `json:"id"`
+	Login  string `json:"login"`
+	Avatar string `json:"avatar_url"`
 }
 
 type GitLabels struct {
@@ -29,9 +31,10 @@ type GitLabels struct {
 }
 
 type OutputNode struct {
-	User     GitUsers    `json:"user"`
-	Lables   []GitLabels `json:"labels"`
-	TotalPrs int32       `json:"totalprs"`
+	User            GitUsers    `json:"user"`
+	Lables          []GitLabels `json:"labels"`
+	TotalPrs        int32       `json:"totalprs"`
+	TotalPrsWLabels int32
 }
 
 var sessionPulls = []Gitpulls{}
@@ -43,13 +46,14 @@ func showWhatsHiding() {
 	})
 	fmt.Println("Analizing open PRs statistic...")
 	currentId := sessionPulls[0].GitUser.Id
-	outputNodes = append(outputNodes, OutputNode{User: sessionPulls[0].GitUser, TotalPrs: 0})
+	outputNodes = append(outputNodes, OutputNode{User: sessionPulls[0].GitUser, TotalPrs: 0, TotalPrsWLabels: 0})
 
 	for _, pull := range sessionPulls {
 		if pull.GitUser.Id == currentId {
 			outputNodes[len(outputNodes)-1].TotalPrs++
 			if len(pull.Labels) > 0 {
 				outputNodes[len(outputNodes)-1].Lables = append(outputNodes[len(outputNodes)-1].Lables, pull.Labels...)
+				outputNodes[len(outputNodes)-1].TotalPrsWLabels++
 			}
 		} else {
 			currentId = pull.GitUser.Id
@@ -57,20 +61,22 @@ func showWhatsHiding() {
 		}
 	}
 
-	fmt.Println(len(outputNodes))
+	fmt.Println("Most productive contributors: ")
+	for _, node := range outputNodes {
+		fmt.Println(node.User.Login + " made " + fmt.Sprintf("%d", node.TotalPrs) + " PRs, of which " + fmt.Sprintf("%d", node.TotalPrsWLabels) + " were made with labels")
+		createUserHTML(node.User.Login, testRequest(node))
+	}
 
-	testRequest(outputNodes)
+	fmt.Println("I have a strong feeling, that you should check out your Christmas Tree :)")
 
 }
 
-func testRequest(pull []OutputNode) string {
+func testRequest(pull OutputNode) []byte {
 
-	payload, err := json.Marshal(outputNodes)
+	payload, err := json.Marshal(pull)
 	if err != nil {
 		log.Panic(err)
 	}
-
-	fmt.Println(string(payload))
 
 	req, err := http.NewRequest("POST", os.Getenv("Raven"), bytes.NewBuffer(payload))
 	if err != nil {
@@ -79,7 +85,7 @@ func testRequest(pull []OutputNode) string {
 	req.Header.Add("Content-Type", "application/json")
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return "Cannot establish connection with Raven server"
+		log.Fatal("Cannot establish connection with Raven server")
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
@@ -87,6 +93,14 @@ func testRequest(pull []OutputNode) string {
 		log.Panic(err)
 	}
 
-	result := string(body)
+	result := decode64(string(body))
+	return result
+}
+
+func decode64(base64 string) []byte {
+	result, err := b64.StdEncoding.DecodeString(base64)
+	if err != nil {
+		log.Panic(err)
+	}
 	return result
 }
